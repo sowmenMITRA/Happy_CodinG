f532e100a360e4a100c7adf2c9ff1331abac4856
Sowmen Mitra <42622201+sowmenMITRA@users.noreply.github.com>
2020-04-28 20:05:55 +0600

    pointer to array

--- pointer to array.c
+++ pointer to array.c
@@ -1,1 +1,17 @@
-{\rtf1}
+#include<stdio.h>
+int main()
+{
+    int arr[5];
+    int *ptr=arr;
+    for(int loop=0; loop<5; loop++)
+    {
+        printf("Enter Element [%d] : ",loop+1);
+        scanf("%d",ptr+loop);
+    }
+    for(int loop=0; loop<5; loop++)
+    {
+        printf("Value of [%d] element is %d and Address is %p.\n",loop+1,*ptr,ptr);
+        ptr++;
+    }
+    return 0 ;
+}
