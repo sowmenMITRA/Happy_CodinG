707f42b76d6be3e947b7b699a7f60befd1e089d3
Sowmen Mitra <42622201+sowmenMITRA@users.noreply.github.com>
2020-05-24 19:29:29 +0600

    count nodes

---
+++ Linked_List/9.Count nodes.c
@@ -0,0 +1,111 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+struct node
+{
+    int data ;  // setting data
+    struct node *next;  // setting address of next node
+}*head;
+void CreatNodeList(int n);
+void PrintData();
+void ToCount();
+
+int main()
+{
+    int TotalNode,data;
+    printf("Enter how many Nodes you want to create : ");
+    scanf("%d",&TotalNode);
+
+    CreatNodeList(TotalNode);    // calling function to create node items.
+    printf("Node List created successfully\n\n");
+
+    printf("Node Data's are\n");
+    PrintData();
+
+    ToCount();
+
+    PrintData(); //to print the final node list.
+}
+
+void CreatNodeList(int n)
+{
+    int data;
+    struct node *new_node,*temp;
+
+    /* creating Head note or starting node
+    malloc will return void pointer so need to type cast.
+    and putting the start address of that memory block to
+    a pointer head */
+
+    head=(struct node *)malloc(sizeof(struct node));
+    if(head==NULL)
+    {
+        printf("Unable to allocate memory");
+    }
+    else
+    {
+        printf("Enter Data of Node 1 : ");
+        scanf("%d",&data);
+        head->data=data;      //passing data to the head noad
+        head->next=NULL;
+        temp=head;      //setting address as null
+
+        /*
+        creating other n-1 nodes
+        */
+
+        for(int loop=2; loop<=n; loop++)
+        {
+            new_node=(struct node *)malloc(sizeof(struct node));
+            if(new_node==0)
+            {
+                printf("Unable to allocate memory");
+                break;
+            }
+
+            else
+            {
+                printf("Enter Data of Node %d : ",loop);
+                scanf("%d",&data);
+                new_node->data=data;
+                new_node->next=NULL;
+
+                temp->next=new_node;
+                temp=temp->next;
+            }
+        }
+    }
+}
+
+void PrintData()
+{
+    struct node *temp;
+    int loop=1;
+    temp=head; //we gonna traverse the temp where head is always fixed.
+    if(temp==NULL)
+    {
+        printf("The list is empty");
+    }
+    else
+    {
+        while(temp!=0)
+        {
+            printf("Node %d Data is : %d\n",loop++,temp->data);
+            temp=temp->next;
+        }
+    }
+}
+
+void ToCount()
+{
+   int count=0;
+   struct node *temp;
+   temp=head;
+   while(temp->next!=NULL)
+   {
+       temp=temp->next;
+       count++;
+   }
+   printf("\n\n\tTotal nodes : %d\n",count+1);
+   printf("\n\n");
+}
